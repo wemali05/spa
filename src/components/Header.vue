31fe6c49d6aeb8d811b64d50946114f4fb9b70a2
wemali05 <alfredwemali05@gmail.com>
2020-03-28 11:13:47 +0300

    styling links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,6 @@
 <template>
     <ul class="nav nav-pills">
-        <li class="nav-item">
-            <router-link to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/user">User</router-link>
-        </li>
-     </ul>
+            <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+            <router-link to="/user" tag="li"  active-class="active"><a>User</a></router-link>
+      </ul>
 </template>
