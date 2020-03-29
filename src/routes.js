0bd9e650d3f7e46de61be83850e5af197c6f37ad
wemali05 <alfredwemali05@gmail.com>
2020-03-29 12:25:58 +0300

    lazy load routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,35 @@
 import Home from './components/Home.vue';
-import User from './components/user/User.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
+// import User from './components/user/User.vue';
+// import UserStart from './components/user/UserStart.vue';
+// import UserDetail from './components/user/UserDetail.vue';
+// import UserEdit from './components/user/UserEdit.vue';
+
+// lazy loading routes
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User.vue'));
+    }, 'user');
+};
+
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart.vue'));
+    }, 'user');
+};
+
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail.vue'));
+    }, 'user');
+};
+
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit.vue'));
+    }, 'user');
+};
+
+
 
 export const routes = [{
         path: '',
